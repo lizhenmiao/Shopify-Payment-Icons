8186b9da5c7a3c2db76aa63d3260d6afb6548572
lizhenmiao <431521978@qq.com>
2023-10-25 11:33:07 +0800

    修复项目打开空白404的问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+/* eslint-disable prettier/prettier */
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "./",
 });
